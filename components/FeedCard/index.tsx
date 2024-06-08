dc14f8e299c610895ba25c15b6a3ef2764da7356
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-06-08 13:42:17 +0530

    Implemented the no feature toaster

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -5,17 +5,22 @@ import { FaRetweet } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
 import { Tweet } from "@/gql/graphql";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 interface FeedCardProps {
   data: Tweet
+}
+
+const featureNotImplemented = () => {
+  toast.error("This feature will be implemented soon. Stay tuned!");
+  return;
 }
 
 const FeedCard: React.FC<FeedCardProps> = (props) => {
   // console.log(props);
   const { data } = props
   return (
-
-    <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-4 hover:bg-slate-800 transition-all cursor-pointer">
+    <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-4 hover:bg-slate-900 transition-all cursor-pointer">
       <div className="gap-3 grid grid-cols-12">
         <div className="col-span-1">
           {data.author?.profileImageURL && <Image
@@ -28,9 +33,9 @@ const FeedCard: React.FC<FeedCardProps> = (props) => {
         </div>
         <div className="col-span-11">
           <h5>
-            <Link href={`/${data.author?.id}`}>{data.author?.firstName} {data.author?.lastName}</Link>
+            <Link href={`/${data.author?.id}`} className="font-semibold">{data.author?.firstName} {data.author?.lastName}</Link>
           </h5>
-          <p>
+          <p className="mb-3">
             {data.content}
           </p>
           {
@@ -38,16 +43,16 @@ const FeedCard: React.FC<FeedCardProps> = (props) => {
           }
           <div className="p-2 w-[90%] flex justify-between mt-5 text-xl items-center">
             <div>
-              <BiMessageRounded />
+              <BiMessageRounded onClick={featureNotImplemented}/>
             </div>
             <div>
-              <FaRetweet />
+              <FaRetweet onClick={featureNotImplemented}/>
             </div>
             <div>
-              <AiOutlineHeart />
+              <AiOutlineHeart onClick={featureNotImplemented}/>
             </div>
             <div>
-              <BiUpload />
+              <BiUpload onClick={featureNotImplemented}/>
             </div>
           </div>
         </div>
